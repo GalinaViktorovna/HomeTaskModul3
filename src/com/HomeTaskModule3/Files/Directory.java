453a1d7751f4ba875f40411a73c9be1fb6ce1252
GalinaViktorovna <gkalenskaya@gmail.com>
2016-06-21 14:28:38 +0300

    corrected mistakes

--- src/com/HomeTaskModule3/Files/Directory.java
+++ src/com/HomeTaskModule3/Files/Directory.java
@@ -3,31 +3,14 @@ package com.HomeTaskModule3.Files;
 import java.util.List;
 
 public class Directory {
-    private List<Pictures>pictures;
-    private List<Audio>audios;
-    private List<Text>texts;
 
-    public List<Pictures> getPictures() {
-        return pictures;
+    static List<File> directory;
+
+
+    public static void main(String[] args) {
+
+
     }
 
-    public void setPictures(List<Pictures> pictures) {
-        this.pictures = pictures;
-    }
-
-    public List<Audio> getAudios() {
-        return audios;
-    }
-
-    public void setAudios(List<Audio> audios) {
-        this.audios = audios;
-    }
-
-    public List<Text> getTexts() {
-        return texts;
-    }
-
-    public void setTexts(List<Text> texts) {
-        this.texts = texts;
-    }
 }
+
--- src/com/HomeTaskModule3/Files/File.java
+++ src/com/HomeTaskModule3/Files/File.java
@@ -1,4 +1,17 @@
 package com.HomeTaskModule3.Files;
 
 public class File {
+    protected String name;
+
+
+    public File file() {
+        return new File();
+    }
+
+    public File edit(File file) {
+        return file;
+    }
+
+    public void delete(File file) {
+    }
 }
