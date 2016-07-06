b73cd1e960b221d692978968ec6dbc725d4d2c79
GalinaViktorovna <gkalenskaya@gmail.com>
2016-07-06 21:39:46 +0300

    corrected name of methods

--- src/com/HomeTaskModule3/MusicTools/Shipping.java
+++ src/com/HomeTaskModule3/MusicTools/Shipping.java
@@ -22,26 +22,26 @@ public class Shipping {
         return order;
     }
 
-    public List<MusicTool> prepareListOfMusicToolsToRemove(MusicToolsShop musicToolsShop, Map<String, Integer> order) throws ExceptionInName, ExceptionOfQuantityInOrder {
+    public List<MusicTool> prepareListToRemove(MusicToolsShop musicToolsShop, Map<String, Integer> order) throws ExceptionInName, ExceptionOfQuantityInOrder {
         List<MusicTool> result = new ArrayList<>();
 
         for (Map.Entry<String, Integer> orderEntry : order.entrySet()) {
             String musicToolType = orderEntry.getKey();
-            Integer numberOfMusicToolToBeRemoved = orderEntry.getValue();
-            int numberOfMusicToolRemoved = 0;
+            Integer MusicToolToBeRemoved = orderEntry.getValue();
+            int MusicToolRemoved = 0;
             for (MusicTool musicTool : musicToolsShop.getMusicToolsShop()) {
-                if (musicTool.getName().equals(musicToolType) && numberOfMusicToolRemoved < numberOfMusicToolToBeRemoved) {
+                if (musicTool.getName().equals(musicToolType) && MusicToolRemoved < MusicToolToBeRemoved) {
                     result.add(musicTool);
-                    numberOfMusicToolRemoved++;
+                    MusicToolRemoved++;
                 } else if ((!musicTool.getName().equals("Guitar")) && (!musicTool.getName().equals("Trumpet")) && (!musicTool.getName().equals("Piano"))) {
                     throw new ExceptionInName("Type of music tool must be \"Guitar\" or \" \"Trumpet\" or \"Piano\"!!!", musicTool.getName());
                 }
             }
-            if (numberOfMusicToolRemoved < numberOfMusicToolToBeRemoved) {
+            if (MusicToolRemoved < MusicToolToBeRemoved) {
                 throw new IllegalArgumentException("Shop does not have enough " + musicToolType + "s");
             }
-            if (numberOfMusicToolToBeRemoved <= 0) {
-                throw new ExceptionOfQuantityInOrder("Quantity in order can not be < 1", numberOfMusicToolToBeRemoved);
+            if (MusicToolToBeRemoved <= 0) {
+                throw new ExceptionOfQuantityInOrder("Quantity in order can not be < 1", MusicToolToBeRemoved);
             }
         }
 
@@ -49,16 +49,16 @@ public class Shipping {
     }
 
     public void removeMusicToolsFromTheShop(MusicToolsShop musicToolsShop, Map<String, Integer> order) {
-        for (Map.Entry<String, Integer> orderEntry : order.entrySet()) {
+        for (Map.Entry<String, Integer> orderEntry: order.entrySet()) {
             String musicToolType = orderEntry.getKey();
-            Integer numberOfMusicToolsToBeRemoved = orderEntry.getValue();
-            int numberOfMusicToolsRemoved = 0;
+            Integer MusicToolsToBeRemoved = orderEntry.getValue();
+            int MusicToolsRemoved = 0;
             Iterator<MusicTool> iterator = musicToolsShop.getMusicToolsShop().iterator();
             while (iterator.hasNext()) {
                 MusicTool musicTool = iterator.next();
-                if (musicTool.getName().equals(musicToolType) && numberOfMusicToolsRemoved < numberOfMusicToolsToBeRemoved) {
+                if (musicTool.getName().equals(musicToolType) && MusicToolsRemoved < MusicToolsToBeRemoved) {
                     iterator.remove();
-                    numberOfMusicToolsRemoved++;
+                    MusicToolsRemoved++;
                 }
             }
         }
@@ -66,7 +66,7 @@ public class Shipping {
 
     public void startUp(Shipping shipping, MusicToolsShop musicToolsShop) {
         try {
-            List<MusicTool> musicToolsToBeRemove = shipping.prepareListOfMusicToolsToRemove(musicToolsShop, shipping.getOrder());
+            List<MusicTool> musicToolsToBeRemove = shipping.prepareListToRemove(musicToolsShop, shipping.getOrder());
             shipping.removeMusicToolsFromTheShop(musicToolsShop, shipping.getOrder());
             System.out.println("Order: " + musicToolsToBeRemove);
             System.out.println(musicToolsShop.toString());
